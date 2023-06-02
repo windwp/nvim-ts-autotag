2548506269af422ff9cc78f27a8b481bc3c81f76
Paul Buzakov <buzakovpa@gmail.com>
2023-06-02 08:47:41 +0500

    * Try to fix internal.lua

--- lua/nvim-ts-autotag.lua
+++ lua/nvim-ts-autotag.lua
@@ -3,23 +3,28 @@ local internal = require("nvim-ts-autotag.internal")
 local M = {}
 
 function M.init()
-  require "nvim-treesitter".define_modules {
-    autotag = {
-      module_path = 'nvim-ts-autotag.internal',
-      is_supported = function(lang)
-        return internal.is_supported(lang)
-      end
+    local status, plugin = pcall(require, 'nvim-treesitter')
+    if (not status) then
+        return
+    end
+
+    plugin.define_modules {
+        autotag = {
+            module_path = 'nvim-ts-autotag.internal',
+            is_supported = function(lang)
+                return internal.is_supported(lang)
+            end
+        }
     }
-  }
 end
 
 function M.setup(opts)
-  internal.setup(opts)
-  vim.cmd[[augroup nvim_ts_xmltag]]
-  vim.cmd[[autocmd!]]
-  vim.cmd[[autocmd FileType * call v:lua.require('nvim-ts-autotag.internal').attach()]]
-  vim.cmd[[autocmd BufDelete * lua require('nvim-ts-autotag.internal').detach(vim.fn.expand('<abuf>'))]]
-  vim.cmd[[augroup end]]
+    internal.setup(opts)
+    vim.cmd [[augroup nvim_ts_xmltag]]
+    vim.cmd [[autocmd!]]
+    vim.cmd [[autocmd FileType * call v:lua.require('nvim-ts-autotag.internal').attach()]]
+    vim.cmd [[autocmd BufDelete * lua require('nvim-ts-autotag.internal').detach(vim.fn.expand('<abuf>'))]]
+    vim.cmd [[augroup end]]
 end
 
 return M
